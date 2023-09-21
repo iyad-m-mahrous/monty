21626d2ca86dfbed7506ac18e493c3160f963caf
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-20 22:32:20 -0700

    Fixing betty Swap

--- swap.c
+++ swap.c
@@ -12,13 +12,13 @@ void _swap(stack_t **stack, unsigned int line_number)
 	int temp;
 
 	if (*stack)
-	       if ((*stack)->next)
-	       {
-		       temp = (*stack)->n;
-		       (*stack)->n = (*stack)->next->n;
-		       (*stack)->next->n = temp;
-		       return;
-	       }
+		if ((*stack)->next)
+		{
+			temp = (*stack)->n;
+			(*stack)->n = (*stack)->next->n;
+			(*stack)->next->n = temp;
+			return;
+		}
 	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 	_free_all(stack);
 	exit(EXIT_FAILURE);
