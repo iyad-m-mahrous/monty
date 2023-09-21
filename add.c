919f39d8e83798065d90b994690fe013493f4d61
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-20 22:21:20 -0700

    Task 4 add

--- add.c
+++ add.c
@@ -10,12 +10,12 @@
 void _add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack)
-	       if ((*stack)->next)
-	       {
-		       (*stack)->next->n += (*stack)->n;
-		       _pop(stack, line_number);
-		       return;
-	       }
+		if ((*stack)->next)
+		{
+			(*stack)->next->n += (*stack)->n;
+			_pop(stack, line_number);
+			return;
+		}
 	fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 	_free_all(stack);
 	exit(EXIT_FAILURE);
