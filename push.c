eab50c823dd8937d0074a3f4780b5a4d65e95506
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-21 00:29:52 -0700

    testing Task 1

--- push.c
+++ push.c
@@ -10,26 +10,28 @@
 void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *pointer;
-	int arg_num = 1, i = 0, is_int = 1;
+	int arg_num = 1, j = 0, flag = 0;
 
 	if (shared.arg)
 	{
-		arg_num = atoi(shared.arg);
-		if (arg_num < 0)
-			i++;
-		while (shared.arg[i] != '\0')
+		if (shared.arg[0] == '-')
+			j++;
+		for (; shared.arg[j] != '\0'; j++)
 		{
-			if (shared.arg[i] < 48 || shared.arg[i] > 57)
-				is_int = 0;
-			i++;
-		}
-	}
-	if (!shared.arg || !is_int)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		_free_all(stack);
-		exit(EXIT_FAILURE);
-	}
+			if (shared.arg[j] > 57 || shared.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			fclose(shared.file);
+			free(shared.line_buff);
+			_free_stack(stack);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fclose(shared.file);
+		free(shared.line_buff);
+		_free_stack(stack);
+		exit(EXIT_FAILURE); }
 	pointer = (stack_t *) malloc(sizeof(*pointer));
 	if (!pointer)
 	{
