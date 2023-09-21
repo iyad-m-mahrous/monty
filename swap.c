27a43b752d83810acd2365170873cf55c98a4436
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-20 22:05:53 -0700

    Task 3 Swap

--- swap.c
+++ swap.c
@@ -11,13 +11,15 @@ void _swap(stack_t **stack, unsigned int line_number)
 {
 	int temp;
 
-	if (!(*stack) || !(*stack)->next)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		_free_all(stack);
-		exit(EXIT_FAILURE);
-	}
-	temp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = temp;
+	if (*stack)
+	       if ((*stack)->next)
+	       {
+		       temp = (*stack)->n;
+		       (*stack)->n = (*stack)->next->n;
+		       (*stack)->next->n = temp;
+		       return;
+	       }
+	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+	_free_all(stack);
+	exit(EXIT_FAILURE);
 }
